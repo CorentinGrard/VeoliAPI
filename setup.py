c17a07942c756a53264f8b4e0a9dbd7d6bdc63fe
Corentin Grard <corentin.grard@ibm.com>
2024-08-13 08:55:15 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="veoliAPI",
-    version="0.0.1",
+    version="0.0.2",
     author="Corentin Grard",
     author_email="corentin.grard@gmail.com",
     description="To get the monthly water consumption from the Veolia API",
